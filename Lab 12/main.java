f277cea600a78a25f3a9f461068f712956f2f1f1
Chris V <creevue@gmail.com>
2023-08-07 21:31:22 -0400

    Reader

--- Lab 12/main.java
+++ Lab 12/main.java
@@ -4,9 +4,9 @@ import java.io.*;
 public class main {
     public static void main(String[] args) {
         JFileChooser fileChooser = new JFileChooser();
-        
+
         int choice = fileChooser.showOpenDialog(null);
-        
+
         if (choice == JFileChooser.APPROVE_OPTION) {
             File selectedFile = fileChooser.getSelectedFile();
             processFile(selectedFile);
@@ -14,21 +14,21 @@ public class main {
             System.out.println("No file selected.");
         }
     }
-    
+
     public static void processFile(File file) {
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
             String fileName = file.getName();
             int lineCount = 0;
             int wordCount = 0;
             int charCount = 0;
-            
+
             String line;
             while ((line = reader.readLine()) != null) {
                 lineCount++;
                 wordCount += countWords(line);
                 charCount += line.length();
             }
-            
+
             System.out.println("Summary Report:");
             System.out.println("File Name: " + fileName);
             System.out.println("Number of Lines: " + lineCount);
